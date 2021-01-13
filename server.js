dd64445d3db303784ba042c6d9d2c2cbbffdeea5
suzygiv <suzannegivnish@gmail.com>
2021-01-13 12:23:47 -0500

    deploying

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
+    );
 
 // routes
 app.use(require("./routes/api.js"));
